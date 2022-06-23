6fc834111fea94755a9b90e51d7c589b04609fed
‘YusukeUemura01’ <xrayu0022@gmail.com>
2022-06-23 16:13:03 +0900

    create_accout_page.dartを修正2

--- lib/screens/create_accout_page.dart
+++ lib/screens/create_accout_page.dart
@@ -8,8 +8,7 @@ import 'package:sandbox_level1/utils/function_utils.dart';
 import 'package:sandbox_level1/view_model/create_account_page_controller.dart';
 
 final createAccountPageProvider = StateNotifierProvider<
-    CreateAccountPageStateController,
-    CreateAccountPageState>((ref) {
+    CreateAccountPageStateController, CreateAccountPageState>((ref) {
   return CreateAccountPageStateController(CreateAccountPageState(
     nameController: TextEditingController(),
     emailController: TextEditingController(),
@@ -17,11 +16,8 @@ final createAccountPageProvider = StateNotifierProvider<
   ));
 });
 
-
 class CreateAccountPage extends HookConsumerWidget {
-
   CreateAccountPage({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -34,72 +30,71 @@ class CreateAccountPage extends HookConsumerWidget {
             width: double.infinity,
             child: Column(
               children: [
-              const SizedBox(
-              height: 50,
-            ),
-            GestureDetector(
-              onTap: () async {
-                final pickedFile = await FunctionUtils().getImageGallery(); //imagePickerで画像取得
-                ref.read(createAccountPageProvider.notifier).changeIconImage(File(pickedFile.path));
-              },
-              child: CircleAvatar(
-                foregroundImage: createAccountPageState.iconImage == null
-                    ? NetworkImage(createAccountPageState.imagePath)
-                    : FileImage(createAccountPageState.iconImage!) as ImageProvider,
-                radius: 40,
-                child: const Icon(Icons.add),
-              ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: createAccountPageState.nameController,
-                decoration: InputDecoration(
-                    hintText: "名前",
-                    errorText: createAccountPageState.nameErrorText),
-              ),
-            ),
-            Container(
-              width: 300,
-              child: TextField(
-                controller: createAccountPageState.emailController,
-                decoration: InputDecoration(
-                  hintText: "メールアドレス",
-                  errorText: createAccountPageState.emailErrorText,
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-            ),
-            Container(
-            width: 300,
-            child: TextField(
-              controller: createAccountPageState.passController,
-              decoration: InputDecoration(
-                  hintText: "パスワード",
-                  errorText: createAccountPageState.passErrorText),
+                GestureDetector(
+                  onTap: () async {
+                    final pickedFile = await FunctionUtils().getImageGallery(); //imagePickerで画像取得
+                    ref.read(createAccountPageProvider.notifier).changeIconImage(File(pickedFile.path));
+                  },
+                  child: CircleAvatar(
+                    foregroundImage: createAccountPageState.iconImage == null
+                        ? NetworkImage(createAccountPageState.imagePath)
+                        : FileImage(createAccountPageState.iconImage!)
+                            as ImageProvider,
+                    radius: 40,
+                    child: const Icon(Icons.add),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  child: TextField(
+                    controller: createAccountPageState.nameController,
+                    decoration: InputDecoration(
+                        hintText: "名前",
+                        errorText: createAccountPageState.nameErrorText),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  child: TextField(
+                    controller: createAccountPageState.emailController,
+                    decoration: InputDecoration(
+                      hintText: "メールアドレス",
+                      errorText: createAccountPageState.emailErrorText,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  child: TextField(
+                    controller: createAccountPageState.passController,
+                    decoration: InputDecoration(
+                        hintText: "パスワード",
+                        errorText: createAccountPageState.passErrorText),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    ref.read(createAccountPageProvider.notifier).initializeErrorText(); //エラーテキスト初期化
+                    final checkController = ref.read(createAccountPageProvider.notifier).checkControllerText(//空欄がないかをチェック
+                            createAccountPageState.nameController!.text,
+                            createAccountPageState.passController!.text,
+                            createAccountPageState.emailController!.text
+                    );
+                    if (!checkController) return; //エラーがあったとき
+                  },
+                  child: const Text("登録"),
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 50,
-          ),
-          ElevatedButton(
-              onPressed: () async {
-                ref.read(createAccountPageProvider.notifier).initializeErrorText();//エラーテキスト初期化
-                final checkController = ref.read(createAccountPageProvider.notifier).checkControllerText(//空欄がないかをチェック
-                    createAccountPageState.nameController!.text,
-                    createAccountPageState.passController!.text,
-                    createAccountPageState.emailController!.text
-                );
-
-                if(!checkController)return;//エラーがあったとき
-
-              },
-              child: const Text("登録"))
-          ],
         ),
       ),
-    ),)
-    ,
     );
   }
 }
-
